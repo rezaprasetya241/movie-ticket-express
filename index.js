f777cfab719b50dd02f0ec0dc25319175fa9135b
rezaprasetya241 <rezaprasetya241@gmail.com>
2023-07-02 13:42:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const port = 8080;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  try {
+    res.send("hello");
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`SERVER RUNNING ON PORT ${port}`);
+});
